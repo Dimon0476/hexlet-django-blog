2a53b037bdf591d8fd1981049bd0f9434d1082ea
Dimon0476 <dmitry.fodorov@gmail.com>
2023-11-25 16:33:42 +0200

    models, migrations

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import redirect, reverse
 
 # Create your views here.
 from django.http import HttpResponse
 
-def index(request):
-    tags = ['Articles', 'статьи', 'набор статей', 'python', 'oop']
-    return render(
-        request,
-        'articles/index.html',
-        context={'tags': tags},
-    )
+
+def index(request, tags=None, article_id=None):
+    article_id = 42
+    tags = 'python'
+
+    return render(request, 'articles/index.html', context={'article_id': article_id, 'tags': tags})
+    # return redirect('article')
--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
+
 
 class HomePageView(TemplateView):
 
@@ -9,6 +10,10 @@ class HomePageView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['who'] = 'World'
         return context
+
+
+def index(request):
+    return redirect('article', kwargs={'article_id': 42, 'tags': 'python'})
 
 # для страницы about
 def about(request):
